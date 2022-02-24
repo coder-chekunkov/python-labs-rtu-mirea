6a57f127e76c134eed17d954fcc7117f3650a27c
coder-chekunkov@yandex.ru <coder-chekunkov@yandex.ru>
2022-02-25 00:03:28 +0300

    solved problems from practice_4

---
+++ practice_4/4_9/main.py
@@ -0,0 +1,12 @@
+# 4.9: "Миниатюра для сайта"
+from PIL import Image
+
+
+def make_preview(size, colors):
+    img = Image.open("image.jpg")
+    img = img.resize(size)
+    img = img.quantize(colors)
+    img.save('res.bmp')
+
+
+make_preview((200, 200), 4)
